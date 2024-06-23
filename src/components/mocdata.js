7fd53bbc56ae1566cdf0528d55ff039db9cf02b5
ABHINAV GUPTA <abhinav.gupta.1220@gmail.com>
2024-06-24 01:15:10 +0530

    Add files via upload

---
+++ src/components/mocdata.js
@@ -0,0 +1,14 @@
+
+const articles = [
+      {
+      "source": {
+        "id": null,
+        "name": "Livemint"
+      },
+      "title": "NEWS Headlines",
+      "urlToImage": "https://t4.ftcdn.net/jpg/02/71/53/89/360_F_271538920_QfQaUsbICjT8sLFBesUU1MOY6xSDUx9l.jpg",
+      "publishedAt": "2024-06-22T15:02:43Z",
+    }
+]
+
+export default articles
