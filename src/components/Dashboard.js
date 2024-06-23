203eaf9068660955a6bd6f7579bc0efe1f713be3
ABHINAV GUPTA <abhinav.gupta.1220@gmail.com>
2024-06-24 01:11:24 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Cards from './Cards';
+import articles from './mocdata'
 
 function Dashboard() {
-  const [newslist, setNewslist] = useState([]);
+  const [newslist, setNewslist] = useState(articles);
   const [category, setCategory] = useState('business'); // Set initial category to 'business'
-  const [error, setError] = useState(null); // State to track errors
 
   const handleCategoryChange = (event) => {
     const newCategory = event.target.value.toLowerCase(); // Get the selected category from the dropdown
@@ -12,19 +12,10 @@ function Dashboard() {
   };
 
   const fetchData = async (category) => {
-    try {
-      setNewslist([]); // Clear the current newslist
-      const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=da74144b0a4f42b5b871e5db39bfcbce`);
-      const data = await response.json();
-      if (response.ok) {
-        setNewslist(data.articles);
-      } else {
-        throw new Error(data.message || 'Failed to fetch news');
-      }
-    } catch (err) {
-      console.error('Error fetching data:', err);
-      setError(err.message); // Set error message to state
-    }
+    setNewslist([]); // Clear the current newslist
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=da74144b0a4f42b5b871e5db39bfcbce`);
+    const data = await response.json();
+    setNewslist(data?.articles);
   };
 
   useEffect(() => {
@@ -34,6 +25,7 @@ function Dashboard() {
   return (
     <>
       <div className="filter">
+        
         <div>
           <select className="category-selector" onChange={handleCategoryChange} value={category}>
             <option value="business">Business</option>
@@ -46,12 +38,11 @@ function Dashboard() {
         </div>
       </div>
       <div className="dashboard">
-        {error && <p>Error: {error}</p>} {/* Display error message */}
         {newslist
           .filter(article => article.urlToImage)
           .map((article) => (
             <Cards 
-              key={article.url} // Use article.url instead of article.source.id for unique keys
+              key={article.source.id}
               title={article.title}
               urlToImage={article.urlToImage}
               name={article.source.name}
@@ -60,6 +51,8 @@ function Dashboard() {
             />
           ))}
       </div>
+
+      
     </>
   );
 }
