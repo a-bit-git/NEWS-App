5915530e374a01ea009b888f4c3296ac81f598c9
ABHINAV GUPTA <abhinav.gupta.1220@gmail.com>
2024-06-23 20:20:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
