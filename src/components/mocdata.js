fdba92c3a9e078cf86e05041a934757d08230fb0
ABHINAV GUPTA <abhinav.gupta.1220@gmail.com>
2024-06-24 11:55:36 +0530

    Update mocdata.js

--- src/components/mocdata.js
+++ src/components/mocdata.js
@@ -1,4 +1,3 @@
-
 const articles = [
       {
       "source": {
@@ -8,7 +7,25 @@ const articles = [
       "title": "NEWS Headlines",
       "urlToImage": "https://t4.ftcdn.net/jpg/02/71/53/89/360_F_271538920_QfQaUsbICjT8sLFBesUU1MOY6xSDUx9l.jpg",
       "publishedAt": "2024-06-22T15:02:43Z",
-    }
+      },
+      {
+      "source": {
+        "id": null,
+        "name": "Livemint"
+      },
+      "title": "NEWS Headlines",
+      "urlToImage": "https://t4.ftcdn.net/jpg/02/71/53/89/360_F_271538920_QfQaUsbICjT8sLFBesUU1MOY6xSDUx9l.jpg",
+      "publishedAt": "2024-06-22T15:02:43Z",
+      },
+      {
+      "source": {
+        "id": null,
+        "name": "Livemint"
+      },
+      "title": "NEWS Headlines",
+      "urlToImage": "https://t4.ftcdn.net/jpg/02/71/53/89/360_F_271538920_QfQaUsbICjT8sLFBesUU1MOY6xSDUx9l.jpg",
+      "publishedAt": "2024-06-22T15:02:43Z",
+      }
 ]
 
 export default articles
