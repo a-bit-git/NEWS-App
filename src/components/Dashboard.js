d5f51d8754b4d34894d793f5137bcaa9a4c0687d
ABHINAV GUPTA <abhinav.gupta.1220@gmail.com>
2024-06-24 12:22:42 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,33 +1,112 @@
+// import React, { useState, useEffect } from 'react';
+// import Cards from './Cards';
+// import articles from './mocdata'
+
+// function Dashboard() {
+//   const [newslist, setNewslist] = useState(articles);
+//   const [category, setCategory] = useState('business'); // Set initial category to 'business'
+
+//   const handleCategoryChange = (event) => {
+//     const newCategory = event.target.value.toLowerCase(); // Get the selected category from the dropdown
+//     setCategory(newCategory); // Set the new category
+//   };
+
+//   const fetchData = (category) => {
+//     setTimeout(async ()=>{
+//       const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=da74144b0a4f42b5b871e5db39bfcbce`);
+//       const data = await response.json();
+//       setNewslist(data?.articles);
+//     },500)
+//   };
+
+//   useEffect(() => {
+//     fetchData(category); // Fetch data on initial render and whenever the category changes
+//   }, [category]); // Re-run the effect when the category changes
+
+//   return (
+//     <>
+//       <div className="filter">
+        
+//         <div>
+//           <select className="category-selector" onChange={handleCategoryChange} value={category}>
+//             <option value="business">Business</option>
+//             <option value="entertainment">Entertainment</option>
+//             <option value="health">Health</option>
+//             <option value="science">Science</option>
+//             <option value="sports">Sports</option>
+//             <option value="technology">Technology</option>
+//           </select>
+//         </div>
+//       </div>
+//       <div className="dashboard">
+//         {newslist
+//           .filter(article => article.urlToImage)
+//           .map((article) => (
+//             <Cards 
+//               key={article.source.id}
+//               title={article.title}
+//               urlToImage={article.urlToImage}
+//               name={article.source.name}
+//               publishedAt={article.publishedAt}
+//               url={article.url}
+//             />
+//           ))}
+//       </div>
+
+      
+//     </>
+//   );
+// }
+
+// export default Dashboard;
+
 import React, { useState, useEffect } from 'react';
 import Cards from './Cards';
 import articles from './mocdata'
 
 function Dashboard() {
+
+
+  // const [newslist, setNewslist] = useState(articles);
+  // const [category, setCategory] = useState('business'); // Set initial category to 'business'
+
+  // const handleCategoryChange = (event) => {
+  //   const newCategory = event.target.value.toLowerCase(); // Get the selected category from the dropdown
+  //   setCategory(newCategory); // Set the new category
+  // };
+
+  // const fetchData = (category) => {
+  //   setTimeout(async ()=>{
+  //     const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=da74144b0a4f42b5b871e5db39bfcbce`);
+  //     const data = await response.json();
+  //     setNewslist(data?.articles);
+  //   },500)
+  // };
+
+  // useEffect(() => {
+  //   fetchData(category); // Fetch data on initial render and whenever the category changes
+  // }, [category]); // Re-run the effect when the category change
+
+
   const [newslist, setNewslist] = useState(articles);
-  const [category, setCategory] = useState('business'); // Set initial category to 'business'
 
-  const handleCategoryChange = (event) => {
-    const newCategory = event.target.value.toLowerCase(); // Get the selected category from the dropdown
-    setCategory(newCategory); // Set the new category
-  };
-
-  const fetchData = (category) => {
+  const fetchData = () => {
     setTimeout(async ()=>{
-      const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=da74144b0a4f42b5b871e5db39bfcbce`);
+      const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=da74144b0a4f42b5b871e5db39bfcbce`);
       const data = await response.json();
       setNewslist(data?.articles);
     },500)
   };
 
   useEffect(() => {
-    fetchData(category); // Fetch data on initial render and whenever the category changes
-  }, [category]); // Re-run the effect when the category changes
+    fetchData();
+  }, []);
 
   return (
     <>
       <div className="filter">
         
-        <div>
+        {/* <div>
           <select className="category-selector" onChange={handleCategoryChange} value={category}>
             <option value="business">Business</option>
             <option value="entertainment">Entertainment</option>
@@ -36,14 +115,14 @@ function Dashboard() {
             <option value="sports">Sports</option>
             <option value="technology">Technology</option>
           </select>
-        </div>
+        </div> */}
       </div>
       <div className="dashboard">
         {newslist
           .filter(article => article.urlToImage)
-          .map((article) => (
+          .map((article, index) => (
             <Cards 
-              key={article.source.id}
+              key={article.source.id + index}
               title={article.title}
               urlToImage={article.urlToImage}
               name={article.source.name}
